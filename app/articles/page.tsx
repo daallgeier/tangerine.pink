4b70f53646ffc29781074a1aff156401b1301020
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-11 22:45:46 +0300

    updating articles

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -38,16 +38,16 @@ export default async function ProjectsPage() {
 		);
 
 	return (
-		<Suspense fallback={<p>Loading projects...</p>}>
+		<Suspense fallback={<p>Loading articles...</p>}>
 		<div className="relative pb-16">
 			<Navigation />
 			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
 				<div className="max-w-2xl mx-auto lg:mx-0">
 					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-						Projects
+						Articles
 					</h2>
 					<p className="mt-4 text-zinc-400">
-						My projects, investments, and partnerships are listed here.
+						Read up on my articles, sharings, and ideas.
 					</p>
 				</div>
 				<div className="w-full h-px bg-zinc-800" />
