647438118f787ed9306670b80a42eddaa7e2fd86
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-12 17:55:50 +0000

    trying to fix articles

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -94,7 +94,7 @@ export const Page = defineDocumentType(() => ({
 
 export default makeSource({
 	contentDirPath: "./content",
-	documentTypes: [Page, Project],
+	documentTypes: [Page, Project, Articles],
 	mdx: {
 		remarkPlugins: [remarkGfm],
 		rehypePlugins: [
