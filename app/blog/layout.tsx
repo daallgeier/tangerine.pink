21278d2f8c036d044b47d338c32c86b75c48b625
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-14 13:01:09 +0000

    change "projects" to "blog"

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,4 +1,4 @@
-export default function ProjectsLayout({
+export default function BlogLayout({
 	children,
 }: { children: React.ReactNode }) {
 	return (
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import { allProjects } from "contentlayer/generated";
+import { allBlog } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 import { Article } from "./article";
@@ -11,25 +11,25 @@ import { Suspense } from 'react'
 const redis = Redis.fromEnv();
 
 export const revalidate = 60;
-export default async function ProjectsPage() {
+export default async function BlogPage() {
 	const views = (
 		await redis.mget<number[]>(
-			...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":")),
+			...allBlog.map((p) => ["pageviews", "blog", p.slug].join(":")),
 		)
 	).reduce((acc, v, i) => {
-		acc[allProjects[i].slug] = v ?? 0;
+		acc[allBlog[i].slug] = v ?? 0;
 		return acc;
 	}, {} as Record<string, number>);
 
 
-	const featured = allProjects.find((project) => project.slug === "22dconsulting")!;
-	const top2 = allProjects.find((project) => project.slug === "tangerines-youtube")!;
-	const top3 = allProjects.find((project) => project.slug === "hermine")!;
-	const sorted = allProjects
+	const featured = allBlog.find((blog) => blog.slug === "22dconsulting")!;
+	const top2 = allBlog.find((blog) => blog.slug === "tangerines-youtube")!;
+	const top3 = allBlog.find((blog) => blog.slug === "hermine")!;
+	const sorted = allBlog
 		.filter((p) => p.published)
 		.filter(
-			(project) =>
-				project.slug !== featured.slug
+			(blog) =>
+				blog.slug !== featured.slug
 		)
 		.sort(
 			(a, b) =>
@@ -38,23 +38,23 @@ export default async function ProjectsPage() {
 		);
 
 	return (
-		<Suspense fallback={<p>Loading projects...</p>}>
+		<Suspense fallback={<p>Loading blog...</p>}>
 		<div className="relative pb-16">
 			<Navigation />
 			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
 				<div className="max-w-2xl mx-auto lg:mx-0">
 					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-						Projects
+						Blog
 					</h2>
 					<p className="mt-4 text-zinc-400">
-						My projects, investments, and partnerships are listed here.
+						My blog, investments, and partnerships are listed here.
 					</p>
 				</div>
 				<div className="w-full h-px bg-zinc-800" />
 
 				<div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
 					<Card>
-						<Link href={`/projects/${featured.slug}`}>
+						<Link href={`/blog/${featured.slug}`}>
 							<article className="relative w-full h-full p-4 md:p-8">
 								<div className="flex items-center justify-between gap-2">
 									<div className="text-xs text-zinc-100">
@@ -95,9 +95,9 @@ export default async function ProjectsPage() {
 					</Card>
 
 					<div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-						{[top2, top3].map((project) => (
-							<Card key={project.slug}>
-								<Article project={project} views={views[project.slug] ?? 0} />
+						{[top2, top3].map((blog) => (
+							<Card key={blog.slug}>
+								<Article blog={blog} views={views[blog.slug] ?? 0} />
 							</Card>
 						))}
 					</div>
@@ -108,27 +108,27 @@ export default async function ProjectsPage() {
 					<div className="grid grid-cols-1 gap-4">
 						{sorted
 							.filter((_, i) => i % 3 === 0)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
+							.map((blog) => (
+								<Card key={blog.slug}>
+									<Article blog={blog} views={views[blog.slug] ?? 0} />
 								</Card>
 							))}
 					</div>
 					<div className="grid grid-cols-1 gap-4">
 						{sorted
 							.filter((_, i) => i % 3 === 1)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
+							.map((blog) => (
+								<Card key={blog.slug}>
+									<Article blog={blog} views={views[blog.slug] ?? 0} />
 								</Card>
 							))}
 					</div>
 					<div className="grid grid-cols-1 gap-4">
 						{sorted
 							.filter((_, i) => i % 3 === 2)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
+							.map((blog) => (
+								<Card key={blog.slug}>
+									<Article blog={blog} views={views[blog.slug] ?? 0} />
 								</Card>
 							))}
 					</div>
