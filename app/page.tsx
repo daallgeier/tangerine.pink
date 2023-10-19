2433429b265413ff64201d1d628efc5542c8331d
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 22:14:17 +0200

    change text colour

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 						<Link
 							key={item.href}
 							href={item.href}
-							className="text-sm duration-500 text-zinc-900 hover:text-zinc-600"
+							className="text-sm duration-500 text-zinc-800 hover:text-zinc-500"
 						>
 							{item.name}
 						</Link>
@@ -38,12 +38,12 @@ export default function Home() {
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 ">
+				<h2 className="text-sm text-zinc-800 ">
 					Servus, I am Tangerine, People also call me Dominik. I run {" "}
 					<Link
 						target="_blank"
 						href="https://22dconsulting.com"
-						className="underline duration-500 hover:text-zinc-300"
+						className="underline duration-500 hover:text-zinc-500"
 					>
 						22d consulting
 					</Link>
@@ -53,7 +53,7 @@ export default function Home() {
 					<Link
 						target="_blank"
 						href="https://dahoam.ventures"
-						className="underline duration-500 hover:text-zinc-300"
+						className="underline duration-500 hover:text-zinc-500"
 					>
 						dahoam ventures
 					</Link>{" "}
