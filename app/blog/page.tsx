ef376081d0e493412b6eb7e9e0fd303ddf2a9442
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 12:41:57 +0200

    rollback to bgimage on /blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -38,36 +38,21 @@ export default async function BlogPage() {
 		);
 
 	return (
-		<body style={{ overflowY: "scroll" }}>
-			<Suspense fallback={<p>Loading blog...</p>}>
-				<div
-					style={{
-						backgroundImage: "url('/2023-10-oludeniz1700-sunsetheart.jpg')",
-						backgroundPosition: "center",
-						backgroundSize: "cover",
-						position: "fixed",
-						top: 0,
-						left: 0,
-						right: 0,
-						bottom: 0,
-						zIndex: -1,
-						height: "100vh",
-					}}
-				/>
-				<div className="relative pb-16 bg-gray-900 bg-cover"
-					style={{ zIndex: 1, height: "100vh" }}
-				>
-					<Navigation />
-					<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-						<div className="max-w-2xl mx-auto lg:mx-0">
-							<h2 className="text-3xl font-bold tracking-tight text-zinc-700 sm:text-4xl">
-								Blog
-							</h2>
-							<p className="mt-4 text-zinc-900">
-								Welcome to my Blogs and Articles.  I am sharing my thoughts and experiences on topics like Organisational Security and Privacy as well as updates from my Sports and Adventures.
-							</p>
-						</div>
-						<div className="w-full h-px bg-zinc-100" />
+		<Suspense fallback={<p>Loading blog...</p>}>
+		<div
+            style={{ backgroundImage: "url('/2023-10-oludeniz1700-sunsetheart.jpg')" }}
+            className="relative pb-16 bg-gray-900 bg-cover">
+			<Navigation />
+			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+				<div className="max-w-2xl mx-auto lg:mx-0">
+					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+						Blog
+					</h2>
+					<p className="mt-4 text-zinc-400">
+						Welcome to my Blogs and Articles.  I am sharing my thoughts and experiences on topics like Organisational Security and Privacy as well as updates from my Sports and Adventures.
+					</p>
+				</div>
+				<div className="w-full h-px bg-zinc-800" />
 
 						<div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
 							<Card>
@@ -167,6 +152,6 @@ export default async function BlogPage() {
 					</div>
 				</div>
 			</Suspense>
-		</body>
+
 	);
 }
