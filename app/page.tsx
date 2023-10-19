2a41d5d2ceae0e436e63ba65aa639bdde1532f44
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 12:30:14 +0200

    remove bg-gradient-to-tl

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ const navigation = [
 export default function Home() {
 	return (
 		<div
-			className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black"
 			style={{ backgroundImage: "url('/2023-10-oludeniz1700-sunsetheart.jpg')" }}
+			className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden from-black via-zinc-600/20 to-black"
 		>
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex items-center justify-center gap-4">
