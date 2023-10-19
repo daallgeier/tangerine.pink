78965036a1609d13d3a32e64360b172073da1a8f
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 12:41:57 +0200

    update bgimage on /blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -39,7 +39,9 @@ export default async function BlogPage() {
 
 	return (
 		<Suspense fallback={<p>Loading blog...</p>}>
-		<div className="relative pb-16">
+		<div
+            style={{ backgroundImage: "url('/2023-10-oludeniz1700-sunsetheart.jpg')" }}
+            className="relative pb-16 bg-gray-900 bg-cover">
 			<Navigation />
 			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
 				<div className="max-w-2xl mx-auto lg:mx-0">
