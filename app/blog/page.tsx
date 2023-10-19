d72c1f6401a68baa75318d939126518aa9dd00c6
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 18:48:04 +0200

    changing background on header

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -44,7 +44,7 @@ export default async function BlogPage() {
 				className="relative pb-16 bg-gray-900 bg-cover">
 				<Navigation />
 				<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-					<div className="max-w-2xl mx-auto lg:mx-0">
+					<div className="max-w-2xl mx-auto lg:mx-0 bg-zinc-100">
 						<h2 className="text-3xl font-bold tracking-tight text-zinc-700 sm:text-4xl">
 							Blog
 						</h2>
