3661811688f5ec7ac4408e2fc919518871d2340d
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 18:36:14 +0200

    testing 0.9 transparency on cards

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -121,7 +121,7 @@ export default async function BlogPage() {
 														left: 0,
 														right: 0,
 														bottom: 0,
-														backgroundColor: "rgba(0, 0, 0, 0.5)",
+														backgroundColor: "rgba(0, 0, 0, 0.9)",
 													}}
 												/>
 											</div>
