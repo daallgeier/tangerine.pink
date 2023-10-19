11e102613e3e49b64a5a7650a4b117c453823e87
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 18:43:34 +0200

    trying to change the text colour

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -45,10 +45,10 @@ export default async function BlogPage() {
 				<Navigation />
 				<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
 					<div className="max-w-2xl mx-auto lg:mx-0">
-						<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+						<h2 className="text-3xl font-bold tracking-tight text-zinc-700 sm:text-4xl">
 							Blog
 						</h2>
-						<p className="mt-4 text-zinc-400">
+						<p className="mt-4 text-zinc-900">
 							Welcome to my Blogs and Articles.  I am sharing my thoughts and experiences on topics like Organisational Security and Privacy as well as updates from my Sports and Adventures.
 						</p>
 					</div>
