d7e62674442e70e7d4e041698a413926ab56121a
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 19:16:03 +0200

    trying to fix it again

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -53,7 +53,9 @@ export default async function BlogPage() {
 					height: "100vh",
 				}}
 			/>
-			<div className="relative pb-16 bg-gray-900 bg-cover" style={{ zIndex: 1 }}>
+			<div className="relative pb-16 bg-gray-900 bg-cover"
+				style={{ zIndex: 1, overflowY: "scroll", height: "100vh" }}
+			>
 				<Navigation />
 				<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
 					<div className="max-w-2xl mx-auto lg:mx-0">
