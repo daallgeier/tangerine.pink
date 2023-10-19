fecf3e978bc6278190ba875269ac8a8488fc1f8f
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-19 20:47:54 +0200

    remove bgimg on /blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -40,7 +40,7 @@ export default async function BlogPage() {
 	return (
 		<Suspense fallback={<p>Loading blog...</p>}>
 		<div
-            style={{ backgroundImage: "url('/2023-10-oludeniz1700-sunsetheart.jpg')" }}
+            // style={{ backgroundImage: "url('/2023-10-oludeniz1700-sunsetheart.jpg')" }}
             className="relative pb-16 bg-gray-900 bg-cover">
 			<Navigation />
 			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
