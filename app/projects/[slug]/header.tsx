a7bc1892d90c179bf4b6cdc25e0c873699831c70
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-23 12:18:50 +0200

    update social links on /blog/article page

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { ArrowLeft, Eye, Github, Twitter } from "lucide-react";
+import { ArrowLeft, Eye, Github, Instagram, Twitter, } from "lucide-react";
+import { siTiktok } from "simple-icons";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
@@ -76,8 +77,17 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 								} `}
 							/>
 						</Link>
-						<Link target="_blank" href="https://github.com/daallgeier">
-							<Github
+						<Link target="_blank" href="https://instagram.com/da_allgeier">
+							<Instagram
+								className={`w-6 h-6 duration-200 hover:font-medium ${
+									isIntersecting
+										? " text-zinc-400 hover:text-zinc-100"
+										: "text-zinc-600 hover:text-zinc-900"
+								} `}
+							/>
+						</Link>
+						<Link target="_blank" href="https://tiktok.com/@tangerinekugel">
+							<siTiktok
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
 										? " text-zinc-400 hover:text-zinc-100"
