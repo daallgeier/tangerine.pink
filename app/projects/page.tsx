d5eceed0741733ba2d7e1dd5b4cff62463bf7b9f
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-30 13:58:09 +0100

    testing if i found the line height

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -55,7 +55,7 @@ export default async function ProjectsPage() {
 						My projects, investments, and partnerships are listed here.
 					</p>
 				</div>
-				<div className="w-full h-px bg-zinc-800" />
+				<div className="w-full h-3 bg-zinc-800" />
 
 				<div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
 					<Card>
