910d6907c41f994c7e5cd97862d05f258a0ff29a
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-30 14:20:26 +0100

    trying something else

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -9,9 +9,8 @@ type Props = {
 
 export const Article : React.FC<Props> = ({ project, views }) => {
 	return (
-		<Link href={`/projects/${project.slug}`}
-		className="bg-gray-900">
-			<article className="p-4 md:p-8 ">
+		<Link href={`/projects/${project.slug}`}>
+			<article className="p-4 md:p-8 bg-gray-900 bg-fill">
 				<div className="flex justify-between gap-2 items-center">
 					<span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
 						{project.date ? (
