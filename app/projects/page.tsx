001e56e57304ffcc122f57ad3e3201b5ca9ba389
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-30 13:39:08 +0100

    Add background image on /projects /blog first test

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -41,7 +41,15 @@ export default async function ProjectsPage() {
 		<Suspense fallback={<p>Loading projects...</p>}>
 		<div className="relative pb-16">
 			<Navigation />
-			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32"
+				style={{
+					backgroundImage: "url('public/2023-10-oludeniz1700-sunsetheart-compressed.webp')",
+					backgroundPosition: "center",
+					backgroundSize: "cover",
+					backgroundRepeat: "no-repeat",
+					backgroundAttachment: "fixed",
+				}
+				}>
 				<div className="max-w-2xl mx-auto lg:mx-0">
 					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
 						Projects
