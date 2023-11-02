99315b71792d9c3187b2078e97fc619b4078590f
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-11-03 00:04:35 +0100

    change default card colour

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -23,7 +23,7 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<div
 			onMouseMove={onMouseMove}
-			className="overflow-hidden relative duration-700 border rounded-xl bg-zink-800/30 hover:bg-gray-900 group md:gap-8 hover:border-none border-none "
+			className="overflow-hidden relative duration-700 border rounded-xl bg-zink-800/50 hover:bg-gray-900 group md:gap-8 hover:border-none border-none "
 		>
 			<div className="pointer-events-none">
 				<div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
