a0fe9690d720465a503b114be76970c51444a9fc
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-11-03 01:36:29 +0100

    rm gap and add m

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -66,7 +66,7 @@ export default async function ProjectsPage() {
 				</div>
 				<div className="w-full h-3 bg-zinc-800" />
 
-				<div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
+				<div className="grid grid-cols-1  mx-auto lg:grid-cols-2 ">
 					<Card>
 						<Link href={`/projects/${featured.slug}`}>
 							<article className="relative w-full h-full p-4 md:p-8">
