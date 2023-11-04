65f310d4484981e2d29b9b39bfa574c0f1fccbba
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-11-04 14:28:57 +0100

    Fix top position on ProjectsPage background

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -46,7 +46,7 @@ export default async function ProjectsPage() {
 			backgroundSize: "cover",
 			// backgroundAttachment: "fixed",
 			position: "fixed",
-			// top: 0,
+			top: 0,
 			// left: 0,
 			// bottom: 0,
 			// right: 0,
