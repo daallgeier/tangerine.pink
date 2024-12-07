2357398d9dbf296b4312e08e72d876a8998c02cf
daallgeier <hi+github@tangerine.lgbt>
2024-12-07 18:05:28 +0100

    make sure text doesn't fade?

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,11 @@ export default function Home() {
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setFadingOut(true); // Trigger fade-out
+      setFadingOut(true);
 
       setTimeout(() => {
         setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        setFadingOut(false); // Trigger fade-in (by removing fade-out class)
+        setFadingOut(false);
       }, 1000); // Adjust timing to match your CSS transition duration
 
     }, 5000); // Change 5000 to your desired interval in milliseconds
@@ -37,89 +37,95 @@ export default function Home() {
   }, []);
 
   return (
-    <div
-      style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
-      className={`flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gray-900 bg-center bg-cover 
-                 transition-opacity duration-1000 ${fadingOut ? 'opacity-0' : 'opacity-100'}`}
-    >
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-5">
-          {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-lg duration-500 text-white hover:text-zinc-500 font-medium hover:font-bold"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </ul>
-      </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
+    <div className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
+      <div
+        style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
+        className={`absolute inset-0 z-0 bg-gray-900 bg-center bg-cover 
+                   transition-opacity duration-1000 ${fadingOut ? 'opacity-0' : 'opacity-100'}`}
       />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        tangerine 🍊
-      </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-base text-white">
+      {/* Content that should not fade */}
+      <div className="z-10">
+        <nav className="my-16 animate-fade-in">
+          <ul className="flex items-center justify-center gap-5">
+            {navigation.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="text-lg duration-500 text-white hover:text-zinc-500 font-medium hover:font-bold"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
 
-          Servus, I am Tangerine! I cofounded {" "}
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <Particles
+          className="absolute inset-0 -z-10 animate-fade-in"
+          quantity={100}
+        />
+        <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+          tangerine 🍊
+        </h1>
 
-          <Link
-            target="_blank"
-            href="https://22dconsulting.com"
-            className="underline duration-500 hover:text-zinc-500"
-          >
-            22d consulting
-          </Link>
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <div className="my-16 text-center animate-fade-in">
+          <h2 className="text-base text-white">
 
-          {" "}<wbr />and I am investing through {" "}
+            Servus, I am Tangerine! I cofounded {" "}
 
-          <Link
-            target="_blank"
-            href="https://dahoam.ventures"
-            className="underline duration-500 hover:text-zinc-500"
-          >
-            dahoam ventures
-          </Link>
+            <Link
+              target="_blank"
+              href="https://22dconsulting.com"
+              className="underline duration-500 hover:text-zinc-500"
+            >
+              22d consulting
+            </Link>
 
-          {" "}at night. See all my{" "}
+            {" "}<wbr />and I am investing through {" "}
 
-          <Link
-            target=""
-            href="/projects"
-            className="underline duration-500 hover:text-zinc-500"
-          >
-            Projects
-          </Link>
+            <Link
+              target="_blank"
+              href="https://dahoam.ventures"
+              className="underline duration-500 hover:text-zinc-500"
+            >
+              dahoam ventures
+            </Link>
 
-          . <br /><br />Follow my paragliding adventures on{" "}
+            {" "}at night. See all my{" "}
 
-          <Link
-            target="_blank"
-            href="https://youtube.com/@daallgeier"
-            className="underline duration-500 hover:text-zinc-500"
-          >
-            YouTube
-          </Link>
+            <Link
+              target=""
+              href="/projects"
+              className="underline duration-500 hover:text-zinc-500"
+            >
+              Projects
+            </Link>
 
-          {" "} and see all my other socials on the{" "}
-          <Link
-            target=""
-            href="/contact"
-            className="underline duration-500 hover:text-zinc-500"
-          >
-            Contact
-          </Link>
+            . <br /><br />Follow my paragliding adventures on{" "}
 
-          {" "} page.
+            <Link
+              target="_blank"
+              href="https://youtube.com/@daallgeier"
+              className="underline duration-500 hover:text-zinc-500"
+            >
+              YouTube
+            </Link>
 
-        </h2>
+            {" "} and see all my other socials on the{" "}
+            <Link
+              target=""
+              href="/contact"
+              className="underline duration-500 hover:text-zinc-500"
+            >
+              Contact
+            </Link>
+
+            {" "} page.
+
+          </h2>
+        </div>
       </div>
     </div>
   );
