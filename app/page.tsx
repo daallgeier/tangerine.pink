c3009e62b6a1a5b88bf2a04a42cbc0790cba9784
daallgeier <hi+github@tangerine.lgbt>
2024-12-07 18:07:35 +0100

    recentre logo

--- app/page.tsx
+++ app/page.tsx
@@ -5,128 +5,130 @@ import React, { useState, useEffect } from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Projects", href: "/projects" },
-  { name: "Blog", href: "/blog" },
-  { name: "Contact", href: "/contact" },
-  { name: "About", href: "/about" },
+    { name: "Projects", href: "/projects" },
+    { name: "Blog", href: "/blog" },
+    { name: "Contact", href: "/contact" },
+    { name: "About", href: "/about" },
 ];
 
 const images = [
-  '/2023-10-oludeniz1700-sunsetheart-compressed.webp',
-  '/2023-10-oludeniz1700-flyovercloud.webp',
-  //'/image3.png',
-  // Add more image URLs here
+    "/2023-10-oludeniz1700-sunsetheart-compressed.webp",
+    "/2023-10-oludeniz1700-flyovercloud.webp",
+    "/image3.png",
+    // Add more image URLs here
 ];
 
 export default function Home() {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [fadingOut, setFadingOut] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [fadingOut, setFadingOut] = useState(false);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setFadingOut(true);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setFadingOut(true);
 
-      setTimeout(() => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        setFadingOut(false);
-      }, 1000); // Adjust timing to match your CSS transition duration
+            setTimeout(() => {
+                setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+                setFadingOut(false);
+            }, 1000); // Adjust timing to match your CSS transition duration
 
-    }, 5000); // Change 5000 to your desired interval in milliseconds
+        }, 5000); // Change 5000 to your desired interval in milliseconds
 
-    return () => clearInterval(intervalId);
-  }, []);
+        return () => clearInterval(intervalId);
+    }, []);
 
-  return (
-    <div className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
-      <div
-        style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
-        className={`absolute inset-0 z-0 bg-gray-900 bg-center bg-cover 
+    return (
+        <div className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
+            <div
+                style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
+                className={`absolute inset-0 z-0 bg-gray-900 bg-center bg-cover 
                    transition-opacity duration-1000 ${fadingOut ? 'opacity-0' : 'opacity-100'}`}
-      />
+            />
 
-      {/* Content that should not fade */}
-      <div className="z-10">
-        <nav className="my-16 animate-fade-in">
-          <ul className="flex items-center justify-center gap-5">
-            {navigation.map((item) => (
-              <Link
-                key={item.href}
-                href={item.href}
-                className="text-lg duration-500 text-white hover:text-zinc-500 font-medium hover:font-bold"
-              >
-                {item.name}
-              </Link>
-            ))}
-          </ul>
-        </nav>
+            {/* Content that should not fade */}
+            <div className="z-10">
+                <nav className="my-16 animate-fade-in">
+                    <ul className="flex items-center justify-center gap-5">
+                        {navigation.map((item) => (
+                            <Link
+                                key={item.href}
+                                href={item.href}
+                                className="text-lg duration-500 text-white hover:text-zinc-500 font-medium hover:font-bold"
+                            >
+                                {item.name}
+                            </Link>
+                        ))}
+                    </ul>
+                </nav>
 
-        <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-        <Particles
-          className="absolute inset-0 -z-10 animate-fade-in"
-          quantity={100}
-        />
-        <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-          tangerine 🍊
-        </h1>
+                <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+                <Particles
+                    className="absolute inset-0 -z-10 animate-fade-in"
+                    quantity={100}
+                />
+                <div className="flex items-center justify-center"> {/* Added flex container */}
+                    <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+                        tangerine 🍊
+                    </h1>
+                </div> {/* Close flex container */}
 
-        <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-        <div className="my-16 text-center animate-fade-in">
-          <h2 className="text-base text-white">
+                <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+                <div className="my-16 text-center animate-fade-in">
+                    <h2 className="text-base text-white">
 
-            Servus, I am Tangerine! I cofounded {" "}
+                        Servus, I am Tangerine! I cofounded {" "}
 
-            <Link
-              target="_blank"
-              href="https://22dconsulting.com"
-              className="underline duration-500 hover:text-zinc-500"
-            >
-              22d consulting
-            </Link>
+                        <Link
+                            target="_blank"
+                            href="https://22dconsulting.com"
+                            className="underline duration-500 hover:text-zinc-500"
+                        >
+                            22d consulting
+                        </Link>
 
-            {" "}<wbr />and I am investing through {" "}
+                        {" "}<wbr />and I am investing through {" "}
 
-            <Link
-              target="_blank"
-              href="https://dahoam.ventures"
-              className="underline duration-500 hover:text-zinc-500"
-            >
-              dahoam ventures
-            </Link>
+                        <Link
+                            target="_blank"
+                            href="https://dahoam.ventures"
+                            className="underline duration-500 hover:text-zinc-500"
+                        >
+                            dahoam ventures
+                        </Link>
 
-            {" "}at night. See all my{" "}
+                        {" "}at night. See all my{" "}
 
-            <Link
-              target=""
-              href="/projects"
-              className="underline duration-500 hover:text-zinc-500"
-            >
-              Projects
-            </Link>
+                        <Link
+                            target=""
+                            href="/projects"
+                            className="underline duration-500 hover:text-zinc-500"
+                        >
+                            Projects
+                        </Link>
 
-            . <br /><br />Follow my paragliding adventures on{" "}
+                        . <br /><br />Follow my paragliding adventures on{" "}
 
-            <Link
-              target="_blank"
-              href="https://youtube.com/@daallgeier"
-              className="underline duration-500 hover:text-zinc-500"
-            >
-              YouTube
-            </Link>
+                        <Link
+                            target="_blank"
+                            href="https://youtube.com/@daallgeier"
+                            className="underline duration-500 hover:text-zinc-500"
+                        >
+                            YouTube
+                        </Link>
 
-            {" "} and see all my other socials on the{" "}
-            <Link
-              target=""
-              href="/contact"
-              className="underline duration-500 hover:text-zinc-500"
-            >
-              Contact
-            </Link>
+                        {" "} and see all my other socials on the{" "}
+                        <Link
+                            target=""
+                            href="/contact"
+                            className="underline duration-500 hover:text-zinc-500"
+                        >
+                            Contact
+                        </Link>
 
-            {" "} page.
+                        {" "} page.
 
-          </h2>
+                    </h2>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
