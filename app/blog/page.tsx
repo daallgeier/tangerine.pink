6fa14860f755483a5ae0951ed5eb7bd3c397c7f2
daallgeier <hi+github@tangerine.lgbt>
2024-12-14 19:13:00 +0100

    maybe this fixes it this time

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,51 +5,7 @@ import React, { useState, useEffect } from "react";
 import { allBlogs, Blog } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-import { Eye } from "lucide-react";
 import { Suspense } from 'react';
-
-interface Props {
-    blog: Blog;
-    views?: number;
-}
-
-function Article({ blog, views }: Props) {
-    return (
-        <article className="relative flex flex-col p-4 md:p-6 lg:p-8">
-            <div className="flex items-center justify-between gap-2">
-                <div className="text-sm text-zinc-400">
-                    {blog.date ? (
-                        <time dateTime={new Date(blog.date).toISOString()}>
-                            {Intl.DateTimeFormat(undefined, {
-                                dateStyle: "long",
-                            }).format(new Date(blog.date))}
-                        </time>
-                    ) : (
-                        <span> </span>
-                    )}
-                </div>
-                {views !== undefined && (
-                    <span className="flex items-center gap-1 text-sm text-zinc-400">
-                        <Eye className="w-4 h-4" />{" "}
-                        {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
-                    </span>
-                )}
-            </div>
-            <h2 className="mt-4 text-xl font-bold text-white sm:text-2xl font-display">
-                {blog.title}
-            </h2>
-            <p className="mt-4 leading-7 text-zinc-200">{blog.description}</p>
-            <Link
-                href={`/blog/${blog.slug}`}
-                className="mt-4 text-sm font-medium text-white hover:text-zinc-500"
-            >
-                Read more <span aria-hidden="true">&rarr;</span>
-            </Link>
-        </article>
-    );
-}
-
-export const revalidate = 60;
 
 const images = [
     '/2023-10-oludeniz1700-sunsetheart-compressed.webp',
@@ -64,9 +20,19 @@ export default async function BlogPage() {
 
     useEffect(() => {
         const intervalId = setInterval(() => {
+            if (!document.body.contains(document.getElementById('blog-page'))) {
+                clearInterval(intervalId);
+                return;
+            }
+
             setFadingOut(true);
 
             setTimeout(() => {
+                if (!document.body.contains(document.getElementById('blog-page'))) {
+                    clearInterval(intervalId);
+                    return;
+                }
+
                 setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
                 setFadingOut(false);
             }, 1000); // Adjust timing to match your CSS transition duration
@@ -82,10 +48,6 @@ export default async function BlogPage() {
     const top3 = allBlogs.find((blog) => blog.slug === "new-offerings");
     const sorted = allBlogs
         .filter((p) => p.published)
-        /* .filter(
-            (blog) =>
-                blog.slug !== featured.slug
-        ) */
         .sort(
             (a, b) =>
                 new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
@@ -94,7 +56,7 @@ export default async function BlogPage() {
 
     return (
         <Suspense fallback={<p>Loading blog...</p>}>
-            <div className="relative pb-16">
+            <div id="blog-page" className="relative pb-16">
                 <div
                     style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
                     className={`absolute inset-0 z-0 bg-gray-900 bg-center bg-cover 
@@ -114,47 +76,51 @@ export default async function BlogPage() {
                         <div className="w-full h-px bg-zinc-800" />
 
                         <div className="grid grid-cols-1 mx-auto lg:grid-cols-2 ">
-                            <Card>
-                                <Link href={`/blog/${featured.slug}`}>
-                                    <article className="relative w-full h-full p-4 md:p-8">
-                                        <div className="flex items-center justify-between gap-2">
-                                            <div className="text-xs text-zinc-100">
-                                                {featured.date ? (
-                                                    <time dateTime={new Date(featured.date).toISOString()}>
-                                                        {Intl.DateTimeFormat(undefined, {
-                                                            dateStyle: "medium",
-                                                        }).format(new Date(featured.date))}
-                                                    </time>
-                                                ) : (
-                                                    <span> </span>
-                                                )}
+                            {featured && (
+                                <Card>
+                                    <Link href={`/blog/${featured?.slug}`}>
+                                        <article className="relative w-full h-full p-4 md:p-8">
+                                            <div className="flex items-center justify-between gap-2">
+                                                <div className="text-xs text-zinc-100">
+                                                    {featured.date ? (
+                                                        <time dateTime={new Date(featured.date).toISOString()}>
+                                                            {Intl.DateTimeFormat(undefined, {
+                                                                dateStyle: "medium",
+                                                            }).format(new Date(featured.date))}
+                                                        </time>
+                                                    ) : (
+                                                        <span> </span>
+                                                    )}
+                                                </div>
                                             </div>
-                                        </div>
 
-                                        <h2
-                                            id="featured-post"
-                                            className="mt-4 text-3xl font-bold text-white group-hover:text-white sm:text-4xl font-display"
-                                        >
-                                            {featured.title}
-                                        </h2>
-                                        <p className="mt-4 leading-8 duration-150 text-white group-hover:text-white">
-                                            {featured.description}
-                                        </p>
-                                        <div className="absolute bottom-4 md:bottom-8">
-                                            <p className="hidden text-white hover:text-zinc-50 lg:block">
-                                                Read more <span aria-hidden="true">&rarr;</span>
+                                            <h2
+                                                id="featured-post"
+                                                className="mt-4 text-3xl font-bold text-white group-hover:text-white sm:text-4xl font-display"
+                                            >
+                                                {featured.title}
+                                            </h2>
+                                            <p className="mt-4 leading-8 duration-150 text-white group-hover:text-white">
+                                                {featured.description}
                                             </p>
-                                        </div>
-                                    </article>
-                                </Link>
-                            </Card>
+                                            <div className="absolute bottom-4 md:bottom-8">
+                                                <p className="hidden text-white hover:text-zinc-50 lg:block">
+                                                    Read more <span aria-hidden="true">&rarr;</span>
+                                                </p>
+                                            </div>
+                                        </article>
+                                    </Link>
+                                </Card>
+                            )}
 
                             <div className="flex flex-col w-full mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-                                {[top2, top3].map((blog) => (
-                                    <Card key={blog.slug}>
-                                        <Article blog={blog} /> 
-                                    </Card>
-                                ))}
+                                {[top2, top3].map((blog) =>
+                                    blog && (
+                                        <Card key={blog.slug}>
+                                            <Article blog={blog} />
+                                        </Card>
+                                    )
+                                )}
                             </div>
                         </div>
                         <div className="hidden w-full h-px md:block bg-zinc-800" />
